7231ce076f0aa0378290fae03c80698b0c5b78fc
bergano65 <eviten@microsoft.com>
2019-01-08 05:37:44 +0200

    Add project files.

---
+++ IFUniPlayer/IFUniPlayer/GameMenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IFUniPlayer
+{
+    public class GameMenuItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+}
