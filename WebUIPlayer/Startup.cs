836563f8c4933d7a991a400eb45705282fe0de04
bergano65 <eviten@microsoft.com>
2019-04-09 21:31:12 +0300

    web view 0.0

---
+++ WebUIPlayer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebUIPlayer.Startup))]
+namespace WebUIPlayer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
